6aeeea13762a1ea324ffef7c0f05eeb9516bf3ee
Nikola Golijanin <nikola.golijanin@bsdigital.com>
2025-11-04 13:11:10 +0100

    first route

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/api/events", getEvents)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "List of events"})
+}
